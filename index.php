cd9392981cc3c5e04faf5cb820e311be3b00b842
mcvo <macovogaming@gmail.com>
2017-05-25 17:56:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ session_start();
 <html>
 <head>
 <style>table { font-family: arial, sans-serif; border-collapse: collapse; width: 100%; } td, th { border: 1px solid #dddddd; text-align: left; padding: 8px; } tr:nth-child(even) { background-color: #dddddd; }</style>
-<title>The Theme Database`</title>
+<title>The Theme Database</title>
 </head>
 <body>
 
