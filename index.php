65bb407863f3ba966e40e39b619bebfbb4abe6d1
varw2 <b1944386@mvrht.net>
2017-05-25 11:03:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,53 @@
 <?php  
-echo "hell";
-/*
-for ($x = 0; $x <= 700; $x++) {
-  echo "The number is: $x <br>";
-}
-phpinfo(); */
-?>  <title>gogogog</title>
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<style>table { font-family: arial, sans-serif; border-collapse: collapse; width: 100%; } td, th { border: 1px solid #dddddd; text-align: left; padding: 8px; } tr:nth-child(even) { background-color: #dddddd; }</style>
+</head>
+<body>
+
+<table>
+  <tr>
+    <th>Theme Name</th>
+    <th>Description</th>
+    <th>Author</th>
+    <th>Version</th>    
+ </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+    <td>Germany</td>
+    <td>Italy</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+    <td>Mexico</td><td>Italy</td>
+  </tr>
+  <tr>
+    <td>Ernst Handel</td>
+    <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+    <td>Austria</td><td>Italy</td>
+  </tr>
+  <tr>
+    <td>Island Trading</td>
+    <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+    <td>UK</td><td>Italy</td>
+  </tr>
+  <tr>
+    <td>Laughing Bacchus Winecellars</td>
+    <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+    <td>Canada</td><td>Italy</td>
+  </tr>
+  <tr>
+    <td>Magazzini Alimentari Riuniti</td>
+    <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</td>
+    <td>Italy</td><td>Italy</td>
+  </tr>
+</table>
+
+</body>
+</html>
+
